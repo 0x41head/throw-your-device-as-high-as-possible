dea2d79c6a6ebd7a7bfc46cdc685fdfec462e05a
0x41head <aryangiroud@gmail.com>
2024-10-14 15:02:25 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useEffect } from "react";
+import "./App.css";
+
+function App() {
+  useEffect(() => {
+    const acl = new Accelerometer({ frequency: 60 });
+
+    acl.addEventListener("reading", () => {
+      console.log(`Acceleration along the X-axis ${acl.x}`);
+      console.log(`Acceleration along the Y-axis ${acl.y}`);
+      console.log(`Acceleration along the Z-axis ${acl.z}`);
+    });
+
+    console.log("inside use effect ");
+    acl.start();
+  }, []);
+
+  return <>hello</>;
+}
+
+export default App;
