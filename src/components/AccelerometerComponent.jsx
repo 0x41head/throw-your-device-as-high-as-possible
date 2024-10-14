81d00e095c5824cd14eab3b8011a7950900315f1
0x41head <aryangiroud@gmail.com>
2024-10-14 16:52:44 +0530

    MVP

--- src/components/AccelerometerComponent.jsx
+++ src/components/AccelerometerComponent.jsx
@@ -1,22 +1,10 @@
 import { useState } from "react";
 
 function AccelerometerComponent() {
-  const [x, setX] = useState("");
-  const [y, setY] = useState("");
-  const [z, setZ] = useState("");
   const [lax, setLaX] = useState("");
   const [lay, setLaY] = useState("");
   const [laz, setLaZ] = useState("");
-  const acl = new Accelerometer({ frequency: 60 });
-  acl.addEventListener("reading", () => {
-    setX(acl.x);
-    setY(acl.y);
-    setZ(acl.z);
-    console.log(`Acceleration along the X-axis ${acl.x}`);
-    console.log(`Acceleration along the Y-axis ${acl.y}`);
-    console.log(`Acceleration along the Z-axis ${acl.z}`);
-  });
-
+  const [maxLaz, setMaxLaz] = useState(0);
   let laSensor = new LinearAccelerationSensor({ frequency: 60 });
 
   laSensor.addEventListener("reading", (e) => {
@@ -26,18 +14,15 @@ function AccelerometerComponent() {
     console.log(`Linear acceleration along the X-axis ${laSensor.x}`);
     console.log(`Linear acceleration along the Y-axis ${laSensor.y}`);
     console.log(`Linear acceleration along the Z-axis ${laSensor.z}`);
+    if (setMaxLaz < laSensor.z) setMaxLaz(laSensor.z);
   });
   laSensor.start();
-
-  acl.start();
   return (
     <div>
-      <p>x-value = {x}</p>
-      <p>y-value = {y}</p>
-      <p>z-value = {z}</p>
       <p>lx-value = {lax}</p>
       <p>ly-value = {lay}</p>
       <p>lz-value = {laz}</p>
+      <p>max height thrown = {maxLaz ** 2 / (2 * 9.8)}</p>
     </div>
   );
 }
