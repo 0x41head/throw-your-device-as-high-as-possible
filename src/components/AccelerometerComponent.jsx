3f4ea6b9a44632874e47e5b223596a7bd9be3415
0x41head <aryangiroud@gmail.com>
2024-10-14 18:03:01 +0530

    fixed for incompatible browsers

--- src/components/AccelerometerComponent.jsx
+++ src/components/AccelerometerComponent.jsx
@@ -7,21 +7,25 @@ function AccelerometerComponent() {
   const [isBrowserCompatible, setIsBrowserCompatible] = useState(false);
   const [maxLaz, setMaxLaz] = useState(0);
   const [height, setHeight] = useState(0);
+  let laSensor;
+  useEffect(() => {
+    try {
+      laSensor = new LinearAccelerationSensor({ frequency: 60 });
+      setIsBrowserCompatible(true);
+    } catch (error) {
+      console.log(error);
+      setIsBrowserCompatible(false);
+    }
+  }, []);
 
-  try {
-    let laSensor = new LinearAccelerationSensor({ frequency: 60 });
-
+  if (isBrowserCompatible) {
     laSensor.addEventListener("reading", () => {
       setLaX(laSensor.x);
       setLaY(laSensor.y);
       setLaZ(laSensor.z);
     });
     laSensor.start();
-    setIsBrowserCompatible(true);
-  } catch (error) {
-    setIsBrowserCompatible(false);
   }
-
   useEffect(() => {
     if (maxLaz > laz) {
       setHeight((laz / 60) ** 2 / (2 * 9.8));
@@ -38,7 +42,7 @@ function AccelerometerComponent() {
     </div>
   ) : (
     <p>
-      Incompatible device Use a mobile or tablet and ensure you are using a
+      Incompatible device Use a mobile or tablet and ensure you are using a{" "}
       <a href="https://developer.mozilla.org/en-US/docs/Web/API/LinearAccelerationSensor#browser_compatibility">
         compatible browser
       </a>
