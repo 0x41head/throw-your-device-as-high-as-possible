57537d51ec24011f281ffeabfb873a8ebe1d7b2b
0x41head <aryangiroud@gmail.com>
2024-10-14 18:12:36 +0530

    more fixes

--- src/components/AccelerometerComponent.jsx
+++ src/components/AccelerometerComponent.jsx
@@ -1,36 +1,29 @@
 import { useEffect, useState } from "react";
 
 function AccelerometerComponent() {
-  const [lax, setLaX] = useState("");
-  const [lay, setLaY] = useState("");
-  const [laz, setLaZ] = useState("");
-  const [isBrowserCompatible, setIsBrowserCompatible] = useState(false);
+  const [lax, setLaX] = useState(null);
+  const [lay, setLaY] = useState(null);
+  const [laz, setLaZ] = useState(null);
   const [maxLaz, setMaxLaz] = useState(0);
   const [height, setHeight] = useState(0);
-  let laSensor;
-  useEffect(() => {
-    try {
-      laSensor = new LinearAccelerationSensor({ frequency: 60 });
-      setIsBrowserCompatible(true);
-    } catch (error) {
-      console.log(error);
-      setIsBrowserCompatible(false);
-    }
-  }, []);
+  const [isBrowserCompatible, setIsBrowserCompatible] = useState(false);
 
-  if (isBrowserCompatible) {
-    laSensor.addEventListener("reading", () => {
-      setLaX(laSensor.x);
-      setLaY(laSensor.y);
-      setLaZ(laSensor.z);
-    });
-    laSensor.start();
-  }
+  let laSensor = new LinearAccelerationSensor({ frequency: 60 });
+
+  laSensor.addEventListener("reading", () => {
+    setLaX(laSensor.x);
+    setLaY(laSensor.y);
+    setLaZ(laSensor.z);
+  });
+
+  laSensor.start();
+
   useEffect(() => {
     if (maxLaz > laz) {
       setHeight((laz / 60) ** 2 / (2 * 9.8));
       setMaxLaz(laz);
     }
+    if (laz != null) setIsBrowserCompatible(true);
   }, [laz]);
   return isBrowserCompatible ? (
     <div>
@@ -42,7 +35,7 @@ function AccelerometerComponent() {
     </div>
   ) : (
     <p>
-      Incompatible device Use a mobile or tablet and ensure you are using a{" "}
+      Incompatible device Use a mobile or tablet and ensure you are using a
       <a href="https://developer.mozilla.org/en-US/docs/Web/API/LinearAccelerationSensor#browser_compatibility">
         compatible browser
       </a>
