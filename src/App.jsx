c694bb899e6743f75e099f007e4501aa0f34b0b8
0x41head <aryangiroud@gmail.com>
2024-10-14 17:42:10 +0530

    bring back default css

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,7 @@
-import { useEffect } from "react";
 import AccelerometerComponent from "./components/AccelerometerComponent";
-// import "./App.css";
+import "./App.css";
 
 function App() {
-  useEffect(() => {
-    const acl = new Accelerometer({ frequency: 60 });
-
-    acl.addEventListener("reading", () => {
-      console.log(`Acceleration along the X-axis ${acl.x}`);
-      console.log(`Acceleration along the Y-axis ${acl.y}`);
-      console.log(`Acceleration along the Z-axis ${acl.z}`);
-    });
-
-    console.log("inside use effect ");
-    acl.start();
-  });
-
   return <AccelerometerComponent />;
 }
 
